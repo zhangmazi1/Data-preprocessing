cb312ca98ee4b46c90fb1476679ef85d66446b5d
zhangmazi1 <44436968+zhangmazi1@users.noreply.github.com>
2019-04-14 11:00:47 +0800

    Update and rename main-creat - 1h.py to main-Interpolation.py

--- main-Interpolation.py
+++ main-Interpolation.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jan 11 14:51:21 2019
-
-@author: 杨镇宁
+@author: yzn
 """
 import pandas as pd
 import scipy.interpolate as spi
 
-inputfile='C:/Users/杨镇宁/Desktop/work.xlsx'
-
+inputfile='work.xlsx'   #输入文件路径
 #插值过程，采用样条插值，可有效避免拉格朗日插值出现的龙格现象。
 missingData = pd.read_excel(inputfile,sheetname=1,header=None)   #将excel中数据读到dataframe
 row=list(missingData.index)
@@ -21,16 +19,16 @@ for i in missingData.columns:
             x = missingData[i][list(range(j - 50, j)) + list(range(j + 1, j + 51))]#前后各取50个
             x = x[x.notnull()]                    #剔除选取前后数据中空值
             ipo3=spi.splrep(x.index,list(x),k=1)  #横坐标数据必须按大小顺序排列，否则报错
-            missingData[i][j] =spi.splev(j,ipo3)  #试了几种方法，发现k=1为均值插值，比较适合电力数据
+            missingData[i][j] =spi.splev(j,ipo3)  #试了几种方法，发现k=1为均值插值，比较适合数据，根据数据类型试验
             newrow +=[j]
             newcol +=[i]
-missingData.to_excel('插值后数据1小时为单位.xls', header=None, index=False)
+missingData.to_excel('result.xls', header=None, index=False)
 
 #修改插值后单元格颜色过程
 import xlwt
 import xlrd
 from xlutils.copy import copy
-file_name = '插值后数据1小时为单位.xls' #与上面一致
+file_name = 'result.xls' #与上面一致
 n=len(newrow)
 styleBlueBkg = xlwt.easyxf('pattern: pattern solid, fore_colour red;')  #红色
 rb = xlrd.open_workbook(file_name)      #打开t.xls文件
